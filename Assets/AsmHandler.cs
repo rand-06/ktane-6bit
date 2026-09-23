1161af8c9273533b399a0c9cea247919eb943d6b
rand-06 <162802460+rand-06@users.noreply.github.com>
2026-09-23 11:32:47 +0300

    upd

---
+++ Assets/AsmHandler.cs
@@ -0,0 +1,156 @@
+public class AsmHandler : MonoBehaviour{
+    private List<int> reg = new List<int> { 0, 0, 0, 0, 0, 0, 0, 0 };
+    private int counter = 0;
+    private List<char> data = new List<char>();
+
+    private ASM6 asm6;
+
+    private int fromBinary(string bin) => bin.ToCharArray().AsEnumerable().Reverse()
+        .Select((x,i)=>x=='1'?1<<i:0).Sum();
+    private string binToOct(string bin){
+        List<bool> rBin = bin.ToCharArray().AsEnumerable().Reverse().Select(x=>x=='1').ToList();
+        List<char> ans = new List<char>();
+        int m = 0;
+        for (int i=0; i<rBin.Count; i++){
+            if (rBin[i]) m+= 1 << (i%3);
+            if (i%3==2 || i==rBin.Count - 1) {
+                ans.Add("01234567"[m]);
+                m = 0;
+            }
+        }
+        return ans.AsEnumerable().Reverse().Select(x=>x.ToString()).Aggregate((a,b)=>a+b);
+    }
+
+    private bool feed(string bin){
+        if (bin[0] == '1'){
+            int Rd = fromBinary(bin.Substring(4,2))+4;
+            int imm6 = fromBinary(bin.Substring(6,6));
+            switch (fromBinary(bin.Substring(1,3))){
+                case 0: asm6.LDI(Rd, imm6); break;
+                case 1: asm6.ADDI(Rd, imm6); break;
+                case 2: asm6.SUBI(Rd, imm6); break;
+                case 3: asm6.CMPI(Rd, imm6); break;
+                case 4: asm6.ANDI(Rd, imm6); break;
+                case 5: asm6.ORI(Rd, imm6); break;
+                case 6: asm6.XORI(Rd, imm6); break;
+                default: break;
+            }
+        }
+        else{
+            switch (fromBinary(bin.Substring(1,2))){
+                case 0:
+                    int Rd = fromBinary(bin.Substring(6,3));
+                    int Rs = fromBinary(bin.Substring(9,3));
+                    switch(fromBinary(bin.Substring(3,3))){
+                        case 0: asm6.MOV(Rd, Rs); break;
+                        case 1: asm6.ADD(Rd, Rs); break;
+                        case 2: asm6.SUB(Rd, Rs); break;
+                        case 3: asm6.CMP(Rd, Rs); break;
+                        case 4: asm6.AND(Rd, Rs); break;
+                        case 5: asm6.OR(Rd, Rs); break;
+                        case 6: asm6.XOR(Rd, Rs); break;
+                        case 7:
+                            switch(Rs){
+                                case 0: asm6.NOT(Rd); break;
+                                case 1: asm6.LD(Rd); break;
+                                case 2: asm6.ST(Rd); break;
+                                case 3: asm6.LSL(Rd); break;
+                                case 4: asm6.LSR(Rd); break;
+                                case 5: asm6.ROL(Rd); break;
+                                case 6: asm6.ROR(Rd); break;
+                                default: break;
+                            }
+                            break;
+                    }
+                    break;
+                case 1:
+                    int addr = fromBinary(bin.Substring(6,6));
+                    switch(fromBinary(bin.Substring(3,3))){
+                        case 0: asm6.JMP(addr); break;
+                        case 1: asm6.BREQ(addr); break;
+                        case 2: asm6.BRNE(addr); break;
+                        case 3: asm6.BRCS(addr); break;
+                        case 4: asm6.BRCC(addr); break;
+                        case 5: asm6.BRMI(addr); break;
+                        case 6: asm6.BRPL(addr); break;
+                        case 7: asm6.JOP(addr); break;
+                    }
+                    break;
+                case 2:
+                    int Rd = fromBinary(bin.Substring(3,3));
+                    int addr = fromBinary(bin.Substring(6,6));
+                    asm6.DJZ(Rd, addr);
+                    break;
+                default: break;
+            }
+        }
+        if (counter & 63 == 63) return false;
+        counter++;
+    }
+
+    private string commandToString(string bin){
+        if (bin == "000000000000") return "NOP";
+        if (bin[0] == '1'){
+            int Rd = fromBinary(bin.Substring(4,2))+4;
+            string imm6 = binToOct(bin.Substring(6,6));
+            switch (fromBinary(bin.Substring(1,3))){
+                case 0: return $"LDI\t(R{Rd},e{imm6})";
+                case 1: return $"ADDI\t(R{Rd},e{imm6})";
+                case 2: return $"SUBI\t(R{Rd},e{imm6})";
+                case 3: return $"CMPI\t(R{Rd},e{imm6})";
+                case 4: return $"ANDI\t(R{Rd},e{imm6})";
+                case 5: return $"ORI\t(R{Rd},e{imm6})";
+                case 6: return $"XORI\t(R{Rd},e{imm6})";
+                default: return $"e{binToOct(bin)}?";
+            }
+        }
+        else{
+            switch (fromBinary(bin.Substring(1,2))){
+                case 0:
+                    int Rd = fromBinary(bin.Substring(6,3));
+                    int Rs = fromBinary(bin.Substring(9,3));
+                    switch(fromBinary(bin.Substring(3,3))){
+                        case 0: return $"MOV\tR{Rd},R{Rs})";
+                        case 1: return $"ADD\tR{Rd},R{Rs})";
+                        case 2: return $"SUB\tR{Rd},R{Rs})";
+                        case 3: return $"CMP\tR{Rd},R{Rs})";
+                        case 4: return $"AND\tR{Rd},R{Rs})";
+                        case 5: return $"OR\tR{Rd},R{Rs})";
+                        case 6: return $"XOR\tR{Rd},R{Rs})";
+                        case 7:
+                            switch(Rs){
+                                case 0: return $"NOT\tR{Rd}";
+                                case 1: return $"LD\tR{Rd}";
+                                case 2: return $"ST\tR{Rd}";
+                                case 3: return $"LSL\tR{Rd}";
+                                case 4: return $"LSR\tR{Rd}";
+                                case 5: return $"ROL\tR{Rd}";
+                                case 6: return $"ROR\tR{Rd}";
+                                default: return $"e{binToOct(bin)}?";
+                            }
+                            break;
+                    }
+                    break;
+                case 1:
+                    int addr = binToOct(bin.Substring(6,6));
+                    switch(fromBinary(bin.Substring(3,3))){
+                        case 0: return $"JMP\te{addr}";
+                        case 1: return $"BREQ\te{addr}";
+                        case 2: return $"BRNE\te{addr}";
+                        case 3: return $"BRCS\te{addr}";
+                        case 4: return $"BRCC\te{addr}";
+                        case 5: return $"BRMI\te{addr}";
+                        case 6: return $"BRPL\te{addr}";
+                        case 7: return $"JOP\te{addr}";
+                    }
+                    break;
+                case 2:
+                    int Rd = fromBinary(bin.Substring(3,3));
+                    int addr = binToOct(bin.Substring(6,6));
+                    return $"DJZ\tR{Rd},e{addr}";
+                default: return $"e{binToOct(bin)}?";
+            }
+        }
+    }
+
+}
--- Assets/script.cs
+++ Assets/script.cs
@@ -5,210 +5,46 @@ using KModkit;
 
 public class script : MonoBehaviour {
 
-    public TextMesh[] symbols = new TextMesh[64];
-    public TextMesh command;
-    public TextMesh binary;
-    public KMBombInfo info;
-    private int[] reg = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
-    private int[] fin = new int[4] { 0, 0, 0, 0 };
-    private int counter = 0;
-    private int sreg = 0;
+    public List<TextMesh> symbols = new List<TextMesh>();
+    public List<KMSelectable> hovers = new List<KMSelectable>();
+    public KMSelectable button0, button1, commandScreen, binaryScreen;
+    public TextMesh commandText, binaryText;
+    private List<char> data = new List<char>();
+    private List<int> colortable = new List<int>();
+    private List<Color> colors = new List<Color>();
+    private bool ModuleSolved, highlighted;
 
-    //private readonly int[] invgray =
-    //    { 0, 1, 3, 2, 7, 6, 4, 5, 15, 14, 12, 13,
-    //        8, 9, 11, 10, 31, 30, 28, 29, 24, 25,
-    //        27, 26, 16, 17, 19, 18, 23, 22, 20, 21,
-    //        63, 62, 60, 61, 56, 57, 59, 58, 48, 49,
-    //        51, 50, 55, 54, 52, 53, 32, 33, 35, 34,
-    //        39, 38, 36, 37, 47, 46, 44, 45, 40, 41, 43, 42 };
+    private char getChar(int index) => "\tabcdefg0123456789.,!@#$ABCDEFGHIJKLMNOPQRSTUVWXYZ^&*-+=~?<>[]()"[index];
+    
+    void pressSymbol(int index){}
 
-    private const string charTable = "\tabcdefg0123456789.,!@#$ABCDEFGHIJKLMNOPQRSTUVWXYZ^&*-+=~?<>[]()";
-    private const string base36 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private string sn;
-    private int snc = 0;
-    private string data = "";
-    private int[] colortable = new int[64];
-    private Color[] colors = new Color[64];
-
-    int mod(int a, int b)
-    {
-        if (a < 0)
-        {
-            while (a < 0) a += b;
-            return a;
-        }
-        else if (a < b) return a;
-        else
-        {
-            while (a >= b) a -= b;
-            return a;
-        }
-    }
-    string getChar(int num)
-    {
-        return "" + charTable[num];
-    }
-    int getNum(char c)
-    {
-        return charTable.IndexOf(c);
-    }
-
-    void NOP() { }
-    void ADD(int a, int b)
-    {
-        sreg = 
-        reg[a] += reg[b];
-        reg[a] = mod(reg[a], 64);
-
-    }       // REFRESHES SREG
-    void SUB(int a, int b)
-    {
-        reg[a] -= reg[b];
-        reg[a] = mod(reg[a], 64);
-    }       // REFRESHES SREG
-    void MUL(int a, int b)
-    {
-        int ans = reg[a] * reg[b];
-        reg[0] = ans / 64;
-        reg[1] = ans % 64;
-    }
-    void LDS(int a, int B)
-    {
-        reg[a] = getNum(data[B]);
-    }
-    void LDI(int a, int B)
-    {
-        reg[a] = B;
-    }
-    void STS(int a, int B)
-    {
-        data = data.Remove(B, 1).Insert(B, getChar(reg[a]));
-    }
-    void JMP(int A)
-    {
-        counter = A;
-    }
-    void AND(int a, int b)
-    {
-        reg[a] &= reg[b];
-    }       // REFRESHES SREG
-    void OR(int a, int b)
-    {
-        reg[a] |= reg[b];
-    }       // REFRESHES SREG
-    void XOR(int a, int b)
-    {
-        reg[a] ^= reg[b];
-    }       // REFRESHES SREG
-    void OUT(bool n, int fa, int b)
-    {
-        if (n) fin[fa] = 63 - reg[b];
-        else fin[fa] = reg[b];
-    }
-    void LSH(int n, int a)
-    {
-        reg[a] = mod(reg[a] << n, 64);
-    }               // REFRESHES SREG
-    void RSH(int n, int a)
-    {
-        reg[a] >>= n;
-    }       // REFRESHES SREG
-    void ROL(int n, int a)
-    {
-        int ans = (reg[a] + (sreg&2 << 5)) << n;
-        if ((ans & (1 << 6)) != 0) sreg |= 2;
-        else sreg &= 2;
-        for (int i=0; i<n; i++)
-            if ((ans & (1 << 7 + i)) != 0) ans |= (1 << i);
-            else ans &= (1 << i);
-        reg[a] = mod(ans, 64);
-    }       // REFRESHES SREG
-    void ROR(int n, int a)
-    {
-        int ans = reg[a] + (sreg & 2 << 5);
-        int ans2 = (ans >> n) + ((ans - (ans << n))<<(7-n));
-        if ((ans & (1 << 6)) != 0) sreg |= 2;
-        else sreg &= 2;
-        reg[a] = mod(ans2, 64);
-    }       // REFRESHES SREG
-    void BREQ(int A){if ((sreg & (1 << 0)) != 0) JMP(A);}
-    void BRCS(int A){if ((sreg & (1 << 1)) != 0) JMP(A);}
-    void BRMI(int A){if ((sreg & (1 << 2)) != 0) JMP(A);}
-    void BRVS(int A){if ((sreg & (1 << 3)) != 0) JMP(A);}
-    void BRLT(int A){if ((sreg & (1 << 4)) != 0) JMP(A);}
-    void BRIS(int A){if ((sreg & (1 << 5)) != 0) JMP(A);}
-    void BRNE(int A){if ((sreg & (1 << 0)) == 0) JMP(A);}
-    void BRCC(int A){if ((sreg & (1 << 1)) == 0) JMP(A);}
-    void BRPL(int A){if ((sreg & (1 << 2)) == 0) JMP(A);}
-    void BRVC(int A){if ((sreg & (1 << 3)) == 0) JMP(A);}
-    void BRGE(int A){if ((sreg & (1 << 4)) == 0) JMP(A);}
-    void BRIC(int A){if ((sreg & (1 << 5)) == 0) JMP(A);}
-    void SEF(int A) { sreg |= A; }
-    void CLF(int A) { sreg &= A; }
-    void STC(int a, int B)
-    {
-        colortable[B] = reg[a];
-    }
-    void SWP(int a, int b)
-    {
-        int temp = reg[b];
-        reg[b] = reg[a];
-        reg[a] = temp;
-    }
-    //void SETC(int A) { colortable[counter]  = A; }    // too similar to STC r?, mem6
-    void CREG(int A) { sreg = colortable[A];}
-    //void CNRG(int A) { sreg = 63-colortable[A]; }
-    void MOV(int a, int b){ // move Ra into Rb; MOV r0,r0 = NOP.
-        reg[b] = reg[a];
-    }
-    //void ACO(int n, int a)
-    //{
-    //    int ans = reg[a];
-    //    for (int i = 0; i < n; i++) ans ^= (ans >> 1);
-    //    reg[a] = ans;
-    //}
-    //void IACO(int n, int a)
-    //{
-    //    int ans = reg[a];
-    //    for (int i = 0; i < n; i++) ans = invgray[ans];
-    //    reg[a] = ans;
-    //}
-    void WRI(int A)
-    {
-        int ans = (base36.IndexOf(sn[snc%6]) * 36 + base36.IndexOf(sn[(snc + 1)%6]))%64;
-        data = data.Remove(A, 1).Insert(A, getChar(ans));
-        snc++;
-    }
-    void WRIN(int A)
-    {
-        int ans = (base36.IndexOf(sn[snc % 6]) * 36 + base36.IndexOf(sn[(snc + 1) % 6])) % 64;
-        data = data.Remove(A, 1).Insert(A, getChar(63-ans));
-        snc++;
-    }
-
-    // Use this for initialization
-    void Start () {
-        for (int i = 0; i < 64; i++)
-        {
-            colors[i] = new Color(i / 16 / 3f, ((i / 4) % 4) / 3f, (i % 4) / 3f);
-        }
+    void initBoard(){
+        colors = Enumerable.Range(0,64).Select(i => new Color(i / 16 / 3f, ((i / 4) % 4) / 3f, (i % 4) / 3f)).ToList();
         for (int i=0; i<64; i++)
         {
-            string n = getChar(Random.Range(1, 64));
-            symbols[i].text = n;
-            data += n;
-            int c = Random.Range(1, 64);
+            char n = getChar(UnityEngine.Random.Range(1, 64));
+            symbols[i].text = n.ToString();
+            data.Add(n);
+            int c = UnityEngine.Random.Range(1, 64);
             symbols[i].color = colors[c];
-            colortable[i] = c;
+            colortable.Add(c);
+            int i1=i;
+            hovers[i1].OnInteract += delegate{pressSymbol(i1); return false;};
         }
+    }
+
+    void Start () {
+        GetCompomnent<KMSelectable>().OnFocus+=delegate{highlighted = true;};
+        GetCompomnent<KMSelectable>().OnDefocus+=delegate{highlighted = false;};
+        initBoard();
         command.text = "";
         binary.text = "";
-        sn = info.GetSerialNumber().ToUpper();
-        
 	}
 	
-	// Update is called once per frame
+	
 	void Update () {
-		
+		if (ModuleSolved || !highlighted) return;
+        if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Numpad0)) button0.OnInteract();
+        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Numpad1)) button1.OnInteract();
 	}
 }
